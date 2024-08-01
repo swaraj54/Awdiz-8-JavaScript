041ab52a7805de68824d5b6c8cf493b91495f86b
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2024-08-01 10:38:24 +0530

    Updates

--- Day-13/index.js
+++ Day-13/index.js
@@ -33,28 +33,28 @@
 // w : 1
 // r : 1
 // d : 1
-// function findDuplicates(string) {
-//   var output = {};
-//   console.log(string, "string");
-//   for (var i = 0; i <= string.length - 1; i++) {
-//     // console.log(string[i]);
-//     // console.log(output[string[i]]);
+function findDuplicates(string) {
+  var output = {};
+  console.log(string, "string");
+  for (var i = 0; i <= string.length - 1; i++) {
+    // console.log(string[i]);
+    // console.log(output[string[i]]);
 
-//     // if (string[i] == " ") {
-//     //   continue;
-//     // }
-//     if (string[i] != " ") {
-//       if (output[string[i]] == undefined) {
-//         output[string[i]] = 1;
-//       } else {
-//         output[string[i]]++;
-//       }
-//     }
-//   }
-//   console.log(output);
-// }
+    // if (string[i] == " ") {
+    //   continue;
+    // }
+    if (string[i] != " ") {
+      if (output[string[i]] == undefined) {
+        output[string[i]] = 1;
+      } else {
+        output[string[i]]++;
+      }
+    }
+  }
+  console.log(output);
+}
 
-// findDuplicates(string);
+findDuplicates(string);
 
 // While Loop
 
@@ -65,18 +65,18 @@
 
 // Q Print numbers from 1 to given number by using while loop.
 
-// var number = 10;
+var number = 10;
 
-// function print(number) {
-//   var count = 6; // starting
-//   while (count <= number) { //  6 <=  5 -> false
-//     // ending
-//     console.log(count); // 1 2 3 4 5
-//     count++;
-//   }
-// }
+function print(number) {
+  var count = 1; // starting
+  while (count <= number) { //  6 <=  5 -> false
+    // ending
+    console.log(count); // 1 2 3 4 5
+    count++;
+  }
+}
 
-// print(number);
+print(number);
 
 // for(staring ; ending ; sequence){
 
---
+++ Day-14/index.js
@@ -0,0 +1,67 @@
+// // Q Give an aray, do addition of numbers by using while loop .
+
+// var array = [2, 4, 5, 6, 7, 8, 3, 2];
+// // ouput : 120
+
+// function additionOfNumberFromArray(array) {
+//     var sum = 0;
+//     var i = 0;
+//     while( i <= array.length - 1 ){
+//         // console.log(array[i], i);
+//         // if array[i] % 2 === 0
+//         sum += array[i]
+//         i++;
+//     }
+//     // console.log(sum)
+//     return sum ;
+// }
+
+// additionOfNumberFromArray(array);
+
+// Datatype
+// String
+// number
+// boolean
+// undefined
+// varible
+// var
+// let
+// const
+// Hoisting
+// Comparision operators
+// < > <= >= == ===
+// Conditional Statement
+// if else
+// switch
+// logical operators
+// && ||
+// function
+// traditional function -> function functionName() {}
+// arrow function
+// Template literals
+// Array -> to store multiple data
+// Object -> to store multiple data
+// for loop
+// While loop
+
+// Event Loop
+// Node single tread
+
+// function Greeting() {
+//   console.log("Hello.");
+// }
+// console.log("1");
+// setTimeout(Greeting, 5000);
+// console.log("2");
+// setTimeout(() => {
+//   console.log("hello 2");
+// }, 2000);
+
+// setInterval(() => {
+//   console.log("Hello");
+// }, 2000);
+
+
+
+// localStorage -> store, retrive, delete 
+// JSON -> convert to JS, or JS into JSON
